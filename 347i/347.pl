41fcf61b37974b24551fb50ed0c39a43c3edfa1a
Brandon Carr <bcarr@synacor.com>
2018-02-01 15:56:24 -0500

    condense variable assignment

--- 347i/347.pl
+++ 347i/347.pl
@@ -5,22 +5,16 @@ use strict;
 my $filename = "input.txt";
 open(my $fh, "<" ,$filename) or die "Unable to open file: $filename\n";
 
-my $nums;
+my ($nums, $function, $initial, $nsteps);
 my @taps;
-my $function;
-my $initial;
-my $nsteps;
 while (<$fh>){
 	my $line = $_;
 
 	if ($line =~ /\[(\d+,\d+(?:,\d+)*)\] (XOR|XNOR) (\d+) (\d+)/ ){
-		$nums 		= $1;
-		$function 	= $2;
-		$initial 	= $3;
-		$nsteps 	= $4;		
+		($nums, $function, $initial, $nsteps) = ($1, $2, $3, $4);
 	}
-	
-	
 
+	@taps = split(",",$nums);
+	
 }
 close($fh);
