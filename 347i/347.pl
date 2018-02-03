1b948e213d71b61160d3a87b26df277843d69e2f
Brandon Carr <bcarr@synacor.com>
2018-02-03 12:12:03 -0500

    progress

--- 347i/347.pl
+++ 347i/347.pl
@@ -18,22 +18,42 @@ while (<$fh>){
 
 	my @seed = split("", $initial);
 	
-	my $i = 0;
-	my @vals = (0) x scalar @taps;
-	foreach(@taps){
-		my $tap = $_;
-		$vals[$i] += $seed[$tap];
-		$i++;
-	}
-	
-	my $size = scalar @vals;
-	# get initial cur value
-	my $cur = $vals[0] ^ $vals[1];
-	if ($size > 2){
-		for (my $i=2; $i < $size-1; $i++){
-			$cur = $cur ^= $vals[$i];
+	my $seedsize = scalar @seed;
+	if ($function =~ /XOR/){
+		print "0 ";
+		foreach (@seed){print $_};
+		print "\n";
+		my $xorcount = 1;
+		while ($xorcount <= $nsteps){
+			my $i = 0;
+			my @vals = (0) x scalar @taps;
+
+			foreach(@taps){
+				my $tap = $_;
+				$vals[$i] += $seed[$tap];
+				$i++;
+			}
+
+
+			my $size = scalar @vals;
+
+			my $cur = $vals[0] ^ $vals[1];
+
+			if ($size > 2){
+				for (my $i=1; $i < $size-1; $i++){
+					$cur = $cur ^= $vals[$i];
+				}
+			}
+			my $xor = $cur;
+			
+			unshift (@seed, $xor);
+
+			print "$xorcount ";
+			for (my $i=0){print $_};
+			print "\n";	
+			$xorcount++
 		}
 	}
-	print "$cur\n";
+	print "\n";
 }
 close($fh);
